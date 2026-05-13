46ed49128bf9740fb9ba67d49223be6d6abbb383
User <User@PC-5CD021CWMP>
2026-05-13 14:44:11 +0900

    a

---
+++ src/main/java/jp/iglobe/pbl/repository/AccountRepository.java
@@ -0,0 +1,17 @@
+package jp.iglobe.pbl.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import jp.iglobe.pbl.model.Account;
+
+public interface AccountRepository
+extends JpaRepository<Account, Integer> {
+
+    // ログイン用
+    Account findByMail(String mail);
+
+    // 一覧取得
+    List<Account> findAll();
+}
