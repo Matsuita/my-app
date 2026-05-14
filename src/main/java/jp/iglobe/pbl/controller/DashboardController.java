ca87c2ffb4a45aec71e27b3511618b47d09cd141
User <User@WINDOWS-POB8U7O>
2026-05-14 13:28:08 +0900

    co

--- src/main/java/jp/iglobe/pbl/controller/DashboardController.java
+++ src/main/java/jp/iglobe/pbl/controller/DashboardController.java
@@ -3,7 +3,7 @@ package jp.iglobe.pbl.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class DashboardController {
-	@GetMapping("/C0020")
+	@GetMapping("/dashboard")
 	public String dashboard() {
 
 	    return "C0020";
