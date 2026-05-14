1a32b74a05278d50e341d211718170af0ee5e1b7
User <User@WINDOWS-3JF9UBR>
2026-05-14 15:55:28 +0900

    AccountForm追記。島田

--- src/main/java/jp/iglobe/pbl/model/AccountForm.java
+++ src/main/java/jp/iglobe/pbl/model/AccountForm.java
@@ -1,19 +1,32 @@
 package jp.iglobe.pbl.model;
 
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import lombok.Data;
+
 
 
 @Data
 public class AccountForm {
-
+	@Id
+	private Integer accountId;
+	
+	@NotBlank(message = "名前を入力してください")
     private String name;
 
+	@NotBlank(message = "メールアドレスを入力してください")
     private String mail;
 
+	@NotBlank(message = "パスワードを入力してください")
     private String password;
     
+	@NotBlank(message = "確認のためのパスワードを入力してください")
     private String passwordConfirm;
 
-    private String authority;
+	@NotNull(message = "権限を選択してください")
+    private Integer authority;
+	
     
 }
