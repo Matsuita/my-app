20080b0b0113997ec2750ad785b696478e778d84
User <User@WINDOWS-98H3AF2>
2026-05-14 14:09:19 +0900

    売上登録、売上編集確認、売上削除確認＿小山

--- src/main/java/jp/iglobe/pbl/controller/SalesController.java
+++ src/main/java/jp/iglobe/pbl/controller/SalesController.java
@@ -1,25 +1,65 @@
 package jp.iglobe.pbl.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import jp.iglobe.pbl.model.Sales;
+import jp.iglobe.pbl.model.Account;
+import jp.iglobe.pbl.model.Sale;
+import jp.iglobe.pbl.repository.AccountRepository;
+import jp.iglobe.pbl.repository.SalesRepository;
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class SalesController {
-	@GetMapping("/sales")
-	public String newSale(Model model) {
-		model.addAttribute("sale", new Sales());
+	private final AccountRepository accountRepository;
+	private final SalesRepository salesRepository;
+	
+//	売上登録画面
+	@GetMapping("/S0010")
+	public String newSale(Integer accountId, Integer categoryId, Model model) {
+		List<Account> accountList;
+//		List<Categories> categoriesList;
+		accountList = accountRepository.findByAccountId(accountId);
+//		categories = categoriesRepository.findByCategoryId(categoryId);
+		
+		model.addAttribute("sale", new Sale());
+		model.addAttribute("accountList", accountList);
+		model.addAttribute("accountId", accountId);
+		model.addAttribute("categoryId", categoryId);
 		return "S0010";
 	}
 	
-	@PostMapping("/sales/form-confirm")
-	public String salesConfirm(Model model) {
-		model.addAttribute("sale", new Sales());
+//　売上登録確認画面
+	@PostMapping("/S0011")
+	public String salesConfirm(Sale sale, Model model) {
+		model.addAttribute("sale", sale);
 		return "S0011";
 	}
 	
+//　売上登録実行
+	@PostMapping("/sales/create")
+	public String salesCreate(Sale sale) {
+		salesRepository.save(sale);
+		return "redirect:/S0010";
+	}
+	
+//　売上詳細編集確認画面
+	@GetMapping("/S0024")
+	public String editConfirm(Model model) {
+		model.addAttribute("sale", new Sale());
+		return "S0024";
+	}
+	
+//	売上詳細削除確認画面
+	@GetMapping("/S0025")
+	public String salesDelete(Model model) {
+		model.addAttribute("sale", new Sale());
+		return "S0025";
+	}
 
  }
--- src/main/java/jp/iglobe/pbl/repository/SalesRepository.java
+++ src/main/java/jp/iglobe/pbl/repository/SalesRepository.java
@@ -2,8 +2,8 @@ package jp.iglobe.pbl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import jp.iglobe.pbl.model.Sales;
+import jp.iglobe.pbl.model.Sale;
 
-public interface SalesRepository extends JpaRepository<Sales, Integer>{
+public interface SalesRepository extends JpaRepository<Sale, Integer>{
 
 }
