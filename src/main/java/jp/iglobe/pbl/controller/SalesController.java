7649c1d19951e5db28e5136ada5312d0b3e59206
User <User@PC-5CD021CWMP>
2026-05-14 11:21:57 +0900

    a

---
+++ src/main/java/jp/iglobe/pbl/controller/SalesController.java
@@ -0,0 +1,25 @@
+package jp.iglobe.pbl.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jp.iglobe.pbl.model.Sales;
+
+@Controller
+public class SalesController {
+	@GetMapping("/sales")
+	public String newSale(Model model) {
+		model.addAttribute("sale", new Sales());
+		return "S0010";
+	}
+	
+	@PostMapping("/sales/form-confirm")
+	public String salesConfirm(Model model) {
+		model.addAttribute("sale", new Sales());
+		return "S0011";
+	}
+	
+
+ }
