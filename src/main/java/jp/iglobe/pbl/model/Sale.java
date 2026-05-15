b97dd159739f9bf3fd640d7c4ef000fa18c01650
User <User@WINDOWS-98H3AF2>
2026-05-15 13:50:15 +0900

    バリデーション追加

--- src/main/java/jp/iglobe/pbl/model/Sale.java
+++ src/main/java/jp/iglobe/pbl/model/Sale.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -30,27 +31,31 @@ public class Sale {
 	
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	@Column(name = "sale_date")
-	@NotNull(message = "期限を入力してください")
+	@NotNull(message = "販売日を入力してください。")
 	private LocalDate saleDate;
 	
 	@Column(name = "account_id")
+	@NotNull(message = "担当が未選択です。")
 	private Integer accountId;
 	
 	@Column(name = "category_id")
+	@NotNull(message = "商品カテゴリーが未選択です。")
 	private Integer categoryId;
 	
 	@Column(name = "trade_name")
-	@NotBlank(message = "商品名を入力してください")
+	@NotBlank(message = "商品名を入力してください。")
+	@Size(max = 30, message = "商品名が長すぎます。")
 	private String tradeName;
 	
 	@Column(name = "unit_price")
-	@NotNull(message = "単価を入力してください")
+	@NotNull(message = "単価を入力してください。")
 	private Integer unitPrice;
 	
 	@Column(name = "sale_number")
-	@NotNull(message = "個数を入力してください")
+	@NotNull(message = "個数を入力してください。")
 	private Integer saleNumber;
 	
 	@Column(name = "note")
+	@Size(max = 100, message = "備考が長すぎます。")
 	private String note;
 }
