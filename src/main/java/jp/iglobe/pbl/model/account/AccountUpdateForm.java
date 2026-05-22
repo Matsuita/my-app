a879d59b66a1d0e6df62dce15bd27d8a4397997b
User <User@WINDOWS-POB8U7O>
2026-05-22 13:13:26 +0900

    pass

--- src/main/java/jp/iglobe/pbl/model/account/AccountUpdateForm.java
+++ src/main/java/jp/iglobe/pbl/model/account/AccountUpdateForm.java
@@ -25,24 +25,24 @@ public class AccountUpdateForm {
 
 	@NotBlank(message = "パスワードを入力して下さい。")
 	@Size(max = 30, message = "パスワードが長すぎます。")
+	@Pattern(regexp = "^$|(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d]{8,20}$", message = "パスワードは8〜20文字で、大文字・小文字・数字をそれぞれ1つ以上含めてください")
 	private String password;
 	@NotBlank(message = "パスワード（確認）を入力して下さい。")
 	@Transient
 	private String passwordConfirm;
-	
-	@NotNull(message = "売上権限を選択して下さい。")
-    private Integer salesAuthority;
-    
-    @NotNull(message = "アカウント権限を選択して下さい。")
-    private Integer accountsAuthority;
-//	private Integer authority;
 
-	
+	@NotNull(message = "売上権限を選択して下さい。")
+	private Integer salesAuthority;
+
+	@NotNull(message = "アカウント権限を選択して下さい。")
+	private Integer accountsAuthority;
+	//	private Integer authority;
+
 	@AssertTrue(message = "パスワードが一致していません。")
 	public boolean isPasswordValid() {
-	    if (password == null || passwordConfirm == null) {
-	        return false;
-	    }
-	    return password.equals(passwordConfirm);
+		if (password == null || passwordConfirm == null) {
+			return false;
+		}
+		return password.equals(passwordConfirm);
 	}
 }
