c9d6aeddd8b381c52ecfe942c21b7f8b1093068f
User <User@WINDOWS-POB8U7O>
2026-05-22 13:08:28 +0900

    htn

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,4 +1,21 @@
-function toggleMenu() {
-	const menu = document.getElementById("menu");
-	menu.classList.toggle("active");
-}
+document.addEventListener("DOMContentLoaded", () => {
+
+    function setupToggle(inputId, toggleId) {
+        const input = document.getElementById(inputId);
+        const toggle = document.getElementById(toggleId);
+
+        toggle.addEventListener("click", () => {
+            input.classList.toggle("mask");
+
+            if (input.classList.contains("mask")) {
+                toggle.classList.replace("bi-eye-slash", "bi-eye");
+            } else {
+                toggle.classList.replace("bi-eye", "bi-eye-slash");
+            }
+        });
+    }
+
+    setupToggle("password", "togglePassword");
+    setupToggle("passwordConfirm", "togglePasswordConfirm");
+
+});
