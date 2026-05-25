3e5dda2f70dc8be488ae43f10b15c624253e270e
User <User@WINDOWS-3JF9UBR>
2026-05-25 15:30:56 +0900

    アカウント編集にメアドの被りをなくす！！

--- src/main/java/jp/iglobe/pbl/repository/SearchRepository.java
+++ src/main/java/jp/iglobe/pbl/repository/SearchRepository.java
@@ -14,18 +14,18 @@ import jp.iglobe.pbl.model.account.Account;
 public interface SearchRepository extends JpaRepository<Account, Integer> {
 
 	@Query("""
-		    SELECT a FROM Account a
-		    WHERE (:name IS NULL OR a.name LIKE %:name%)
-		    AND (:mail IS NULL OR a.mail LIKE %:mail%)
-		    AND (:salesAuthority IS NULL OR a.salesAuthority IN :salesAuthority)
-		    AND (:accountsAuthority IS NULL OR a.accountsAuthority IN :accountsAuthority)
-		    AND a.isActive = true
-		    """)
-		List<Account> search(
-		    @Param("name") String name,
-		    @Param("mail") String mail,
-		    @Param("salesAuthority") List<Integer> salesAuthority,
-		    @Param("accountsAuthority") List<Integer> accountsAuthority);
+			SELECT a FROM Account a
+			WHERE (:name IS NULL OR a.name LIKE %:name%)
+			AND (:mail IS NULL OR a.mail LIKE %:mail%)
+			AND (:salesAuthority IS NULL OR a.salesAuthority IN :salesAuthority)
+			AND (:accountsAuthority IS NULL OR a.accountsAuthority IN :accountsAuthority)
+			AND a.isActive = true
+			""")
+	List<Account> search(
+			@Param("name") String name,
+			@Param("mail") String mail,
+			@Param("salesAuthority") List<Integer> salesAuthority,
+			@Param("accountsAuthority") List<Integer> accountsAuthority);
 
 	@Transactional
 	@Modifying
@@ -35,5 +35,9 @@ public interface SearchRepository extends JpaRepository<Account, Integer> {
 			WHERE a.accountId = :accountId
 			""") // ← 💡ここでis_activeを 0（false）に更新する
 	void logicalDeleteById(@Param("accountId") Integer accountId);
+
+	//	既に登録されているメールアドレスを弾くための記述
+	// mailカラムに存在するかどうかを真偽値で返す
+	boolean existsByMail(String mail);
 
 }
