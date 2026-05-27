437c3282aafb8f9b146a7401f0a4f7705beb996b
Matsuita <t-matsui@iglobe.co.jp>
2026-05-27 16:50:55 +0900

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO accounts (name, mail, password, sales_authority, accounts_authority, is_active)
-VALUES ('test', 'test@test.com', 'Test1234!', 2, 2, 1);
+VALUES ('test', 'test@test.com', 'Test1234!', 2, 2, true);
