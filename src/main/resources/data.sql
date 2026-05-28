e5bb95240beceee65523702afa8d012fbe1b6a62
Matsuita <t-matsui@iglobe.co.jp>
2026-05-28 09:59:02 +0900

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,17 @@
-
+INSERT INTO sale (
+  sale_date,
+  account_id,
+  category_id,
+  trade_name,
+  unit_price,
+  sale_number,
+  note
+) VALUES (
+  '2026-05-28',
+  6,
+  1,
+  'みかん',
+  500,
+  3,
+  'テスト'
+);
