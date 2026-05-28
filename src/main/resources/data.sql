e2f0ec2af8f46d394168dfe0a2cb045ca8a212b4
Matsuita <t-matsui@iglobe.co.jp>
2026-05-28 09:21:29 +0900

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,12 @@
 ALTER TABLE accounts ADD CONSTRAINT mail UNIQUE (mail);
 
+DELETE FROM accounts
+WHERE id NOT IN (
+  SELECT MIN(id)
+  FROM accounts
+  GROUP BY mail
+);
+
 INSERT INTO accounts (name, mail, password, sales_authority, accounts_authority, is_active)
 VALUES ('admin', 'admin@test.com', 'admin', 2, 2, true)
 ON CONFLICT (mail) DO NOTHING;
